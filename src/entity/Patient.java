fdaa1f364f42ee75e0c44fb656dae8fb72d7399d
naseokhoo <nahoo1202@naver.com>
2025-03-24 14:58:57 +0900

    entity설계완료

---
+++ src/entity/Patient.java
@@ -0,0 +1,57 @@
+package entity;
+
+public class Patient {
+    private String password;
+    private String user_name;
+    private String phone_number;
+    private String active;
+    private int  user_id;
+
+    public Patient(String password, String user_name, String active, int user_id, String phone_number) {
+        this.password = password;
+        this.user_name = user_name;
+        this.active = active;
+        this.user_id = user_id;
+        this.phone_number = phone_number;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public String getActive() {
+        return active;
+    }
+
+    public void setActive(String active) {
+        this.active = active;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+}
